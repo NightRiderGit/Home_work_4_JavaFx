9990dffa82b47c85aa39f1f70898a0f46ca29bf2
Predator <leksys1992@mail.ru>
2021-12-31 18:58:08 +0300

    1. Добавлен разбор методички по 4 уроку 2. Добавлена ДЗ по 4 уроку, в папку home_work_4

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module com.example.home_work_4_javafx {
     requires javafx.fxml;
 
 
-    opens com.example.home_work_4_javafx to javafx.fxml;
-    exports com.example.home_work_4_javafx;
+    opens com.example.lesson_4_JavaFx to javafx.fxml;
+    opens com.example.home_work_4 to javafx.fxml;
+    exports com.example.lesson_4_JavaFx;
+    exports com.example.home_work_4;
 }
